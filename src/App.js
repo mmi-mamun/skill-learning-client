7e1e618e0fd8d92eeefcb2cee0b328298fd8250d
mmi-mamun <muzahidulislam735@gmail.com>
2022-10-25 00:35:19 +0600

    tailwind css framework is selected

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import Header from './components/Header/Header';
 function App() {
   return (
     <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <br />
       <Header></Header>
     </div>
   );
