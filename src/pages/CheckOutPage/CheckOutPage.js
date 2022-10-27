f13f2bae98440994f3a8184d77d454d20e639307
mmi-mamun <muzahidulislam735@gmail.com>
2022-10-27 13:31:59 +0600

    Functional task for checkout page is completed

--- src/pages/CheckOutPage/CheckOutPage.js
+++ src/pages/CheckOutPage/CheckOutPage.js
@@ -1,20 +1,33 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const CheckOutPage = () => {
     const course = useLoaderData();
+    const [displayToast, setDisplayToast] = useState(false);
     console.log(course)
+    const handleSubmitCheckout = (event) => {
+        event.preventDefault();
+        console.log('clicked from checkout');
+        setDisplayToast(true);
+
+    }
     return (
         <div className="mockup-window border bg-base-300">
+
+
             <div className="flex justify-center px-4 py-3 bg-base-200">
+
                 <div className="hero min-h-screen bg-base-200">
                     <div className="hero-content flex-col lg:flex-row-reverse">
+
                         <img src={course.img} className="max-w-sm rounded-lg shadow-2xl" />
 
-                        <div className="hero min-h-screen bg-base-200">
+                        <form onSubmit={handleSubmitCheckout} className="hero min-h-screen bg-base-200">
                             <div className="hero-content flex-col">
                                 <div className="text-center">
                                     <h1 className="text-4xl font-bold">CHECKOUT PAGE</h1>
+                                    <p>Course ID: {course.id}</p>
                                 </div>
 
 
@@ -35,7 +48,7 @@ const CheckOutPage = () => {
                                     </div>
                                 </div>
 
-                                {/*********************** Get Email & Contact Number *******************/}
+                                {/*********************** Get Contact Number & Email*******************/}
                                 <div className="flex gap-2">
                                     <div className="form-control">
                                         <label className="input-group input-group-vertical">
@@ -47,7 +60,7 @@ const CheckOutPage = () => {
                                     <div className="form-control">
                                         <label className="input-group input-group-vertical">
                                             <span>Enter your mail account</span>
-                                            <input type="text" className="input input-bordered" placeholder='user@gmail.com' required />
+                                            <input type="email" className="input input-bordered" placeholder='user@gmail.com' required />
                                         </label>
                                     </div>
                                 </div>
@@ -71,7 +84,7 @@ const CheckOutPage = () => {
                                     <div className="form-control">
                                         <label className="input-group">
                                             <span>City</span>
-                                            <input type="text" className="input input-bordered" />
+                                            <input type="text" className="input input-bordered" required />
                                         </label>
                                     </div>
 
@@ -85,20 +98,20 @@ const CheckOutPage = () => {
                                     <div className="form-control">
                                         <label className="input-group input-group-xs">
                                             <span>State</span>
-                                            <input type="text" className="input input-bordered input-xs" />
+                                            <input type="text" className="input input-bordered input-xs" required />
                                         </label>
                                     </div>
                                     <div className="form-control">
                                         <label className="input-group input-group-xs">
                                             <span>ZIP Code</span>
-                                            <input type="text" className="input input-bordered input-xs" />
+                                            <input type="text" className="input input-bordered input-xs" required />
                                         </label>
                                     </div>
                                 </div>
 
                                 {/*********************** Payment Method  *******************/}
                                 <h3 className='mt-3 text-secondary text-2xl'>Payment Method</h3>
-                                <div className="flex gap-2">
+                                <div className="flex gap-2" required>
                                     <div className="form-control">
                                         <label className="label cursor-pointer">
                                             <input type="radio" name="radio-6" className="radio checked:bg-red-500" checked />
@@ -118,6 +131,34 @@ const CheckOutPage = () => {
                                         </label>
                                     </div>
                                 </div>
+
+
+                                {/*********************** Display Toast After Submit *******************/}
+
+                                {
+                                    displayToast &&
+                                    <div className="toast toast-middle block toast-center transition  duration-200" style={{ width: '500px' }}>
+                                        <div className="alert alert-info">
+                                            <div>
+                                                <div>
+                                                    <p>Course ID : {course.id}</p>
+                                                    <p>Course Name : {course.title}</p>
+                                                    <img src={course.img} alt="" />
+
+                                                    <p>We are processing your student profile for this course...</p>
+                                                    <p>We send the confirmation status in your given mail address...</p>
+                                                    <Link to={'/'}><button className="btn btn-active text-black btn-link">Click to go home page</button></Link>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        {/* <div className="alert alert-success mt-3">
+                                            <div>
+                                                <span>You are successfully added...</span>
+                                            </div>
+                                        </div> */}
+                                    </div>
+
+                                }
 
 
                                 {/*********************** Submit Button  *******************/}
@@ -159,7 +200,7 @@ const CheckOutPage = () => {
                                     </div>
                                 </div> */}
                             </div>
-                        </div>
+                        </form>
                     </div>
 
                 </div>
